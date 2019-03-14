fe6dd0062d7169795fa7f5e0b57e051ae4ac763b
streamich <streamich@gmail.com>
2019-03-14 22:34:08 +0100

    feat: 🎸 add elastic icons

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ getUrl({set: 'ibm_16', icon: 'arrow--down'});
 
 ## License
 
-[Unlicense](LICENSE) &mdash; public domain.
+- `ibm_16` and `ibm_32` icons sets are under [Apache License 2.0 by IBM](https://github.com/IBM/carbon-elements/blob/master/LICENSE)
+- This project is under [Unlicense](LICENSE) license &mdash; public domain.
--- src/__story__/demo.story.tsx
+++ src/__story__/demo.story.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import {Img} from '..';
 
+storiesOf('elastic', module)
+  .add('alert', () => <Img set='elastic' icon='alert' />)
+  .add('arrow_down', () => <Img set='elastic' icon='arrow_down' />)
+  .add('branch', () => <Img set='elastic' icon='branch' />)
+
 storiesOf('ibm_16', module)
   .add('arrow--down', () => <Img set='ibm_16' icon='arrow--down' />)
   .add('download', () => <Img set='ibm_16' icon='download' />)
