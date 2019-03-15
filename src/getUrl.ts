cdea736bb11a027945cebca84b41a8dab06d4ce4
streamich <streamich@gmail.com>
2019-03-15 16:42:07 +0100

    feat: 🎸 update major in CDN URL

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -1,5 +1,5 @@
 import {Icon} from './types';
 
 export const getUrl = ({set, icon}: Icon): string => {
-  return `https://unpkg.com/iconista@1/sets/${set}/${icon}.svg`;
+  return `https://unpkg.com/iconista@2/sets/${set}/${icon}.svg`;
 };
