f2ec28d608a80385f6e6aab2a4065b0c3270b8df
streamich <streamich@gmail.com>
2019-03-15 12:46:04 +0100

    fix: 🐛 use version major in URL

--- src/getUrl.ts
+++ src/getUrl.ts
@@ -1,5 +1,5 @@
 import {Icon} from './types';
 
 export const getUrl = ({set, icon}: Icon): string => {
-  return `https://unpkg.com/iconista@latest/sets/${set}/${icon}.svg`;
+  return `https://unpkg.com/iconista@1/sets/${set}/${icon}.svg`;
 };
