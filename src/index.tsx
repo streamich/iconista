41aebc925fc5d890789a445f6aa208f3153e6ed2
streamich <streamich@gmail.com>
2019-03-15 16:32:00 +0100

    refactor: 💡 remove Promises and simplify <Svg> component

--- src/index.tsx
+++ src/index.tsx
@@ -1,68 +1,46 @@
 import * as React from 'react';
 import {getUrl} from './getUrl';
 import {Icon} from './types';
-import usePromise from 'react-use/lib/usePromise';
+import useRefMounted from 'react-use/lib/useRefMounted';
 
 const {useEffect, useState, useRef} = React;
 
-const loadSvg = (url: string): Promise<Document> => {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.onreadystatechange = () => {
-      if (req.readyState === 4) {
-        const status = req.status;
-        if (status === 200) {
-          // tslint:disable-next-line
-          // console.log(req.responseXML, req.responseText.trim());
-          if (req.responseXML) {
-            resolve(req.responseXML);
-          } else {
-            reject(new Error(`Could not load SVG Document: ${url}`));
-          }
-        } else {
-          reject(new Error(`SVG loading HTTP ${status} error: ${url}`));
-        }
-      }
-    };
-    req.open('GET', url, true);
-    req.send();
-  });
-};
-
 const Svg: React.FunctionComponent<Icon> = (props) => {
   const ref = useRef<SVGSVGElement | null>(null);
-  const mounted = usePromise();
+  const refMounted = useRefMounted();
   const [error, setError] = useState<Error | undefined>();
 
   useEffect(() => {
     const url = getUrl(props);
-    mounted<Document>(loadSvg(url))
-      .then(doc => {
-        const el = ref.current;
-        if (!el) return;
-        const svg = doc.getRootNode().childNodes[0] as SVGSVGElement;
+    const req = new XMLHttpRequest();
+    req.onreadystatechange = () => {
+      if (!refMounted.current) return;
+      const {readyState, status, responseXML: doc} = req;
+      if (readyState !== 4) return;
+      if (status !== 200) return setError(new Error(`SVG loading HTTP ${status} error: ${url}`));
+      if (!doc!) return setError(new Error(`Could not load SVG Document: ${url}`));
 
-        // Set SVG child nodes.
-        while (svg.childNodes.length > 0) {
-          el.appendChild(svg.childNodes[0]);
-        }
+      const el = ref.current;
+      if (!el) return;
 
-        // Set SVG attributes.
-        for (let i = 0; i < svg.attributes.length; i++) {
-          const {name, value} = svg.attributes[i];
-          el.setAttribute(name, value);
-        }
-      })
-      .catch(error => {
-        // tslint:disable-next-line
-        console.error(error);
-        setError(error);
-      });
+      const svg = doc!.getRootNode().childNodes[0] as SVGSVGElement;
+
+      // Set SVG child nodes.
+      while (svg.childNodes.length > 0) el.appendChild(svg.childNodes[0]);
+
+      // Set SVG attributes.
+      for (let i = 0; i < svg.attributes.length; i++) {
+        const {name, value} = svg.attributes[i];
+        el.setAttribute(name, value);
+      }
+    };
+    req.open('GET', url, true);
+    req.send();
   }, [props.set, props.icon]);
 
   if (error) {
     const url = getUrl(props);
-    return <img src={url} />
+    return <img src={url} title={error.message} />;
   }
 
   return <svg ref={ref} />;
